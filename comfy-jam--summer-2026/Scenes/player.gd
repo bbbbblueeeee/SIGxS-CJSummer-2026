7936b44d3c0babbbe2d20d26603ffa17a65f880d
Krystal <krystallimts@gmail.com>
2026-06-07 18:15:46 +0800

    player movement and area detection

--- comfy-jam--summer-2026/Scenes/player.gd
+++ comfy-jam--summer-2026/Scenes/player.gd
@@ -1,9 +1,10 @@
-extends Node
-signal chore_completed(chore)
+extends AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	position.x = 100
+	position.y = 300
 	pass # Replace with function body.
 
 
--- comfy-jam--summer-2026/Scripts/chore.gd
+++ comfy-jam--summer-2026/Scripts/chore.gd
@@ -4,9 +4,12 @@ extends Node2D
 @export var time_taken : int
 @export var point_value : int
 @export var selectable := true
+var player_in_area := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	position.x = 500
+	position.y = 300
 	print(chore_name)
 	print("Time Taken: "+str(time_taken))
 	print("Point Value: "+str(point_value))
@@ -14,11 +17,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if selectable and Input.is_action_pressed("test action"):
+	if selectable and player_in_area and Input.is_action_pressed("object interaction"):
 		chore_completed()
 	pass
 
 func chore_completed():
 	Signals.chore_completed.emit(self)
 	selectable = false
-	
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	player_in_area = true
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	player_in_area = false
