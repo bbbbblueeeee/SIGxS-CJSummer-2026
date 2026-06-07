2cde8cba0701ad8a4cc0584b3d22913dd54a0ea3
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-06-07 18:14:15 +0800

    Implemented Time System
    Implemented a system to track and display the time. Note that this is still missing the check for whether the day has ended or not.

---
+++ comfy-jam--summer-2026/time.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+const END_OF_DAY: int = 22
+@onready var time_display: Label = $time_display
+@export var current_time: int
+@export var displayed_time: int
+@export var time_marker: String
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Signals.chore_completed.connect(on_chore_completed)
+	current_time = 8
+	calculate_time()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func on_chore_completed(chore):
+	current_time += chore.time_taken
+	calculate_time()
+
+func calculate_time():
+	if current_time > 12:
+		displayed_time = current_time - 12
+		time_marker = "PM"
+	else:
+		displayed_time = current_time
+		time_marker = "AM"
+		
+	time_display.text = str(displayed_time) + ":00 " + time_marker
