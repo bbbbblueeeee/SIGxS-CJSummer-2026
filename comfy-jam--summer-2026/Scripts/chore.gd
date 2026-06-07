4acb588d217a49aedaafb738debbb7a7729b594a
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-06-07 14:12:19 +0800

    Made Scripts Folder
    Made a folder for the Scripts, which is separated from the Scenes folder.

---
+++ comfy-jam--summer-2026/Scripts/chore.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var time_component : TimeComponent
+@export var point_component : PointComponent
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
