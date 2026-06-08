121791371451a5e523fa4b96d01bd0deead80233
Krystal <krystallimts@gmail.com>
2026-06-08 17:51:27 +0800

    chore dialogue box
    installed dialogue manager

--- comfy-jam--summer-2026/Scripts/chore.gd
+++ comfy-jam--summer-2026/Scripts/chore.gd
@@ -3,8 +3,9 @@ extends Node2D
 @export var chore_name : String
 @export var time_taken : int
 @export var point_value : int
-@export var selectable : bool = true
 var player_in_area : bool = false
+var is_selected : bool = false
+var completed : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,13 +18,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if selectable and player_in_area and Input.is_action_pressed("object interaction"):
-		chore_completed()
+	if !completed and player_in_area and !is_selected and Input.is_action_just_pressed("object interaction"):
+		is_selected = true
+		show_dialogue_box()
 	pass
+
+func show_dialogue_box():
+	var dialogue = load("res://Scripts/chore.dialogue")
+	var dialogue_box : Node = load("res://Scenes/dialogue_chore.tscn").instantiate()
+	add_child(dialogue_box, true)
+	dialogue_box.start(dialogue,"start")
+
+func deselect():
+	await (get_tree().create_timer(0.5).timeout)
+	is_selected = false
 
 func chore_completed():
 	Signals.chore_completed.emit(self)
-	selectable = false
+	completed = true
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	player_in_area = true
