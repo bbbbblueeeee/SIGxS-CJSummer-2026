e6db0f0d18d0fe37ee14bed2eb83ea7dbde4a100
Phoebe <phoebebangco@gmail.com>
2026-06-19 14:09:14 +0800

    Added Day 0 CGs

--- comfy-jam--summer-2026/Scenes/day_0.gd
+++ comfy-jam--summer-2026/Scenes/day_0.gd
@@ -1,6 +1,8 @@
 extends Node2D
 @onready var scene_1: TextureRect = $scene1
 @onready var scene_2: TextureRect = $scene2
+@onready var scene_3: TextureRect = $scene3
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,6 +22,7 @@ func show_cutscene(day):
 	await Fade.fade(1,0.5).finished
 	show()
 	scene_2.hide()
+	scene_3.hide()
 	await Fade.fade(0,0.5).finished
 	show_dialogue_box()
 
@@ -37,6 +40,12 @@ func show_dialogue_box():
 func change_scene(sceneA, sceneB):
 	sceneB.show()
 	sceneA.hide()
+	
+func change_scene_with_fade(sceneA, sceneB):
+	await Fade.fade(1,0.5).finished
+	sceneB.show()
+	sceneA.hide()
+	await Fade.fade(0,0.5).finished
 
 func end_scene():
 	await Fade.fade(1,0.5).finished
